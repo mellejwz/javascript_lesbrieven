2ef86b997311121428e2aba1a9bca98f7f69e6f7
Melle Zwerver <mellezwerver@outlook.com>
2014-12-08 12:45:55 +0100

    crazy strings

--- lesbrief1/1.3.5/script.js
+++ lesbrief1/1.3.5/script.js
@@ -1,2 +1,14 @@
+var string = "hippopotomonstrosesquippedaliofobie";
+var capital = [true, false];
+var crazyString = string.split("");
+
+for(i=0;i<crazyString.length;i++){
+	var randomChar = crazyString[i];
+	if (capital[Math.round(Math.random())] == true){
+		crazyString[i] = crazyString[i].toUpperCase();
+	}
+	document.getElementById('iets').innerHTML = crazyString.join('');
+}
+
+
 var a = "melle".toUpperCase();
-document.getElementById('iets').innerHTML=a;
---
+++ lesbrief1/1.4/script.js
@@ -0,0 +1,5 @@
+rekenmachine = {
+	merk : 'mellio',
+	type : 'FX-82MZ',
+	
+}
