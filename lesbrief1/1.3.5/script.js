6c3735307f2a55dcd768f712e16001e27a7ddccc
Melle Zwerver <mellezwerver@outlook.com>
2014-12-08 13:27:22 +0100

    removed obselete code

--- lesbrief1/1.3.5/script.js
+++ lesbrief1/1.3.5/script.js
@@ -3,12 +3,11 @@ var capital = [true, false];
 var crazyString = string.split("");
 
 for(i=0;i<crazyString.length;i++){
-	var randomChar = crazyString[i];
 	if (capital[Math.round(Math.random())] == true){
 		crazyString[i] = crazyString[i].toUpperCase();
 	}
+	else{
+		crazyString[i] = crazyString[i].toLowerCase();
+	}
 	document.getElementById('iets').innerHTML = crazyString.join('');
 }
-
-
-var a = "melle".toUpperCase();
